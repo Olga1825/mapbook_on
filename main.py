6ab632b9adca4176b97ed5f4653f1d8a6378ebc6
Olga1825 <olga.nitkiewicz@student.wat.edu.pl>
2026-05-07 21:29:57 +0200

    feat: interactive map of users

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from mapbook_lib.model import users
 
-from mapbook_lib.controler import import read_users, add_user, remove_user, update_uster, update_user_post
+from mapbook_lib.controler import read_users, add_user, remove_user, update_user, update_user_post, get_user_map
 
-def main()
+def main():
 
     while True:
         print("=====MENU======")
@@ -12,6 +12,8 @@ def main()
         print("3 - usuwanie znajomego")
         print("4 - tworzenie posta")
         print("5 - update znajomego")
+        print("6 - mapa lokalizacji znajomych")
+
         choice=input("Wybierz opcję w menu: ")
         print(f"Wybrano opcję {choice}")
         if choice == "0":
@@ -25,8 +27,11 @@ def main()
         if choice == "3":
             remove_user(users)
         if choice == "4":
-            update_user(users)
-        if choice == "5":
             update_user_post(users)
+        if choice == "5":
+            update_user(users)
+        if choice == "6":
+            get_user_map(users)
 
 if __name__ == "__main__":
+    main()
