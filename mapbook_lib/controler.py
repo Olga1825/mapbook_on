4a79be6e59f9b13c08016fa028defe1c6a0a3093
Olga1825 <olga.nitkiewicz@student.wat.edu.pl>
2026-05-17 00:26:54 +0200

    feat: interactive map of users

--- mapbook_lib/controler.py
+++ mapbook_lib/controler.py
@@ -1,3 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+import folium
+
 def read_users(users_data: list) -> None:
     for user in users_data:
         print(f"Twoj znajomy {user['name']} z miejscowosci {user['location']} opublikowal post {user['posts'][-1]}")
@@ -38,6 +42,24 @@ def update_user_post(users_data: list) -> None:
             user["posts"].append(input("Napisz co słychać: "))
             break
 
+def get_cordinates(location: str) -> list:
+    url = f"https://pl.wikipedia.org/wiki/{location}"
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    response_html = BeautifulSoup(response.text, 'html.parser')
+    latitude = float(response_html.select(".latitude")[1].text.replace(",", "."))
+    longitude = float(response_html.select(".longitude")[1].text.replace(",", "."))
+    return [latitude, longitude]
+
 
 def get_user_map(users_data: list) -> None:
-    print("Mapa znajomych będzie dodana później.")
+    m = folium.Map(location=[52.23, 21], zoom_start=6)
+
+    for user in users_data:
+        folium.Marker(
+            location=get_cordinates(user['location']),
+            tooltip=user["name"],
+            popup=user["posts"][-1],
+            icon=folium.Icon(icon="cloud"),
+        ).add_to(m)
+
+    m.save("mapa_znajomych.html")
